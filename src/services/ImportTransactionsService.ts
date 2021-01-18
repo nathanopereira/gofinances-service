86365071a80700f5ea0e30463800f39af0dd45f3
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-01-18 13:44:50 -0300

    atualizando service de importação de transactions

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,60 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
 import Transaction from '../models/Transaction';
 
+import uploadConfig from '../config/upload';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+async function loadCSV(filePath: string): Promise<Request[]> {
+  const readCSVStream = fs.createReadStream(filePath);
+
+  const parseStream = csvParse({
+    ltrim: true,
+    rtrim: true,
+    columns: true,
+  });
+
+  const parseCSV = readCSVStream.pipe(parseStream);
+
+  const lines: Request[] = [];
+
+  parseCSV.on('data', line => lines.push(line));
+
+  await new Promise(resolve => {
+    parseCSV.on('end', resolve);
+  });
+
+  return lines;
+}
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(fileName: string): Promise<Transaction[]> {
+    const csvFilePath = path.join(uploadConfig.directory, fileName);
+
+    const data = await loadCSV(csvFilePath);
+
+    const createTransaction = new CreateTransactionService();
+
+    const transactions = data.map(async ({ type, value, title, category }) => {
+      const transaction = await createTransaction.execute({
+        categoryTitle: category,
+        value,
+        type,
+        title,
+      });
+
+      return transaction;
+    });
+
+    return Promise.all(transactions);
   }
 }
 
