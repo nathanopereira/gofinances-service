e1a7caee6a4c7dc4d4b8f5064699004f18608230
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-01-18 13:43:24 -0300

    atualizando service de criação de transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  categoryTitle: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+
+    let category = await categoryRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    if (!category) {
+      category = categoryRepository.create({
+        title: categoryTitle,
+      });
+
+      await categoryRepository.save(category);
+    }
+
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const balance = await transactionRepository.getBalance();
+
+      if (value > balance.total) {
+        throw new AppError('Insufficient balance');
+      }
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
